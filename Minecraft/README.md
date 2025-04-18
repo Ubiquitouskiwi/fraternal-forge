3df4d277370f306b0e6d9746fb3699c33d994aca
UbiquitousKiwi <tstarling25.ts@gmail.com>
2025-04-18 15:58:20 -0400

    Added beginning of practice addon and dev readme

---
+++ Minecraft/README.md
@@ -0,0 +1,15 @@
+# Development Setup
+
+### Needed Downloads
+- [Visual Studio Code](https://code.visualstudio.com/download)
+- [Bedrock Definitions (VS Code Extention)](https://marketplace.visualstudio.com/items?itemName=destruc7i0n.vscode-bedrock-definitions)
+- [Minecraft Bedrock Development (VS Code Extension)](https://marketplace.visualstudio.com/items?itemName=destruc7i0n.vscode-bedrock-definitions)
+- [Minecraft Samples](https://github.com/Mojang/bedrock-samples/releases)
+
+### Install Addon Script
+This powershell script with copy your resource_pack folders and behavior_pack folders to the development folders in the minecraft installed on this computer. The script will prepend the root folder of your addon to the child folders when copying them to the install folder. You must have your addon setup like the PracticeAddon:
+
+- {Name of Addon}
+  - resource_pack
+  - behavior_pack
+
---
+++ Minecraft/installAddon.ps1
@@ -0,0 +1,43 @@
+# Check for project parameter
+param([string]$project="")
+
+$MINECRAFT_DEVELOPMENT_BEHAVIOR_PACK_FOLDER = "development_behavior_packs"
+$MINECRAFT_DEVELOPMENT_RESOURCE_PACK_FOLDER = "development_resource_packs"
+$MINECRAFT_MAIN_FOLDER = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang"
+$MINECRAFT_PRERELEASE_FOLDER = Join-Path -Path $env:LOCALAPPDATA -ChildPath  "Packages\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\LocalState\games\com.mojang"
+$minecraftFolder = $MINECRAFT_MAIN_FOLDER
+
+$MINECRAFT_ADDON_PROJECTS_ROOT = $PSScriptRoot
+$projectLocalPath = Join-Path -Path $MINECRAFT_ADDON_PROJECTS_ROOT -ChildPath $project
+
+# Check for project parameter
+if ($project -eq "") {
+    Write-Host "No project name specified"
+    Exit
+}
+
+# Check if project  name supplied exists in local git repo
+if (-Not (Test-Path -Path $projectLocalPath)) {
+    Write-Host "Project name supplied doesnt exist in local git repo"
+    Exit
+}
+
+# Check alternative minecraft prerelease folder
+if (-Not (Test-Path -Path $minecraftFolder)) {
+    Write-Host "Couldn't find minecraft main release install folder"
+    if (-Not (Test-Path -Path $MINECRAFT_PRERELEASE_FOLDER)) {
+        Write-Host "Minecraft install cannot be found on computer."
+        Exit
+    }
+    Write-Host "Main release folder not found. switched to prelease folder"
+
+    $minecraftFolder = $MINECRAFT_PRERELEASE_FOLDER
+} 
+
+$projectInstallResourcePath = Join-Path -Path $minecraftFolder -ChildPath (Join-Path -Path $MINECRAFT_DEVELOPMENT_RESOURCE_PACK_FOLDER -ChildPath  "$($project)_resource_pack")
+$projectInstallBehaviorPath = Join-Path -Path $minecraftFolder -ChildPath (Join-Path -Path $project -ChildPath $MINECRAFT_DEVELOPMENT_BEHAVIOR_PACK_FOLDER)
+
+# Check if addon already installed in mincraft install folder
+
+Copy-Item -Path (Join-Path -Path $projectLocalPath -ChildPath (Join-Path -Path "resource_pack" -ChildPath "*")) -Destination (Join-Path -Path $projectInstallResourcePath -ChildPath "$($project)_resource_pack") -Recurse
+Copy-Item -Path (Join-Path -Path $projectLocalPath -ChildPath (Join-Path -Path "resource_pack" -ChildPath "*")) -Destination (Join-Path -Path $projectInstallBehaviorPath -ChildPath "$($project)_resource_pack") -Recurse
