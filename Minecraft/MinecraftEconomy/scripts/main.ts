37b0e4eddc0694b7489e239f90353728afc1b7ed
UbiquitousKiwi <tstarling25.ts@gmail.com>
2025-04-19 16:54:09 -0400

    Added scanning of chests

--- Minecraft/MinecraftEconomy/scripts/main.ts
+++ Minecraft/MinecraftEconomy/scripts/main.ts
@@ -1,8 +1,9 @@
-import { world, system, BlockPermutation, EntityInventoryComponent } from "@minecraft/server";
+import { world, system, BlockPermutation, EntityInventoryComponent, PlayerBreakBlockAfterEvent, PlayerPlaceBlockAfterEvent, PlayerPlaceBlockAfterEventSignal, BlockInventoryComponent } from "@minecraft/server";
 import { MinecraftBlockTypes, MinecraftDimensionTypes } from "@minecraft/vanilla-data";
 
 let ticksSinceLoad = 0;
-let knownBlocks: {[id: string]: number} = {}
+let knownBlocks: {[id: string]: number} = {};
+let chestLocations: {x: number, y: number, z: number}[] = [];
 
 function mainTick() {
   // Keep track of in game time ticks
@@ -16,15 +17,46 @@ function mainTick() {
 
   if (ticksSinceLoad % 100 == 0) {
     scanInventories();
+    scanChests();
+    world.sendMessage(`Total Blocks In Economy. ${JSON.stringify(knownBlocks, null, 4)}`);
   }
 
   system.run(mainTick);
 }
 
+function scanChests() {
+  let overworld = world.getDimension("overworld");
+  for (let location of chestLocations) {
+    let chestBlock = overworld.getBlock(location);
+    if (chestBlock != undefined) {
+      let chestInventory = chestBlock.getComponent("inventory");
+      if  (chestInventory != undefined) {
+        let chestInv = chestInventory as BlockInventoryComponent;
+        if (chestInv.container != undefined) {
+          for (let i = 0; i < chestInv.container.size; i++) {
+            let item = chestInv.container.getItem(i);
+            if (item != undefined) {
+              let modKey = item.typeId.replace("minecraft:", "");
+              if (!(modKey in knownBlocks)) {
+                knownBlocks[modKey] = item.amount;
+              } else {
+                knownBlocks[modKey] += item.amount;
+              }
+            }
+            
+          }
+        }
+        
+      }
+    }
+    
+  }
+}
+
 function scanInventories() {
   let players = world.getPlayers();
   for (let player of players) {
-  
+    let updatedKnownBlocks: { [id: string]: number } = {};
     let playerInventory = player.getComponent("inventory") as EntityInventoryComponent;
     let invContainer = playerInventory.container;
     if (invContainer !== undefined) {
@@ -33,16 +65,17 @@ function scanInventories() {
 
         if (invItem !== undefined) {
           let keyMod = invItem.typeId.replace("minecraft:", "");
-          if (!(keyMod in knownBlocks)) {
+          if (!(keyMod in updatedKnownBlocks)) {
             
-            knownBlocks[keyMod] = invItem.amount;
+            updatedKnownBlocks[keyMod] = invItem.amount;
           } else {
-            knownBlocks[keyMod] += invItem.amount;
+            updatedKnownBlocks[keyMod] += invItem.amount;
           }
         }
       }
     }
-    world.sendMessage(`Total Blocks In Economy. ${JSON.stringify(knownBlocks, null, 4)}`);
+    
+    knownBlocks = updatedKnownBlocks;
   }
 }
 
@@ -71,20 +104,30 @@ function findTopmostBlockUsingPlayer(x: number, z: number) {
   return y;
 }
 
-function initialize() {
-  const overworld = world.getDimension("overworld");
-
-  const spawnLocation = world.getDefaultSpawnLocation();
-  
-  const chest = overworld.getBlock({x: spawnLocation.x + 2, z: spawnLocation.z + 2 , y: findTopmostBlockUsingPlayer(spawnLocation.x + 2, spawnLocation.z + 2)});
-
-  world.sendMessage("Adding chest at x:" + spawnLocation.x + 2 + " y:" + findTopmostBlockUsingPlayer(spawnLocation.x + 2, spawnLocation.z + 2) + " z:" + spawnLocation.z + 2)
-  if (chest === undefined) {
-    console.warn("Could not load the position to place our chest.");
-    return -1;
+function playerPlaceBlockAfterCallback(event: PlayerPlaceBlockAfterEvent) {
+  world.sendMessage("Block placed!");
+  let placedBlock = event.block.typeId.replace("minecraft:", "");
+  if (placedBlock == "chest") {
+    world.sendMessage("Chest placed!! Saving location for scanning.")
+    chestLocations.push({x: event.block.x, y: event.block.y, z: event.block.z});
   }
+}
 
-  chest.setPermutation(BlockPermutation.resolve("chest"));
+function initialize() {
+  // const overworld = world.getDimension("overworld");
+
+  // const spawnLocation = world.getDefaultSpawnLocation();
+  
+  // const chest = overworld.getBlock({x: spawnLocation.x + 2, z: spawnLocation.z + 2 , y: findTopmostBlockUsingPlayer(spawnLocation.x + 2, spawnLocation.z + 2)});
+
+  // world.sendMessage("Adding chest at x:" + spawnLocation.x + 2 + " y:" + findTopmostBlockUsingPlayer(spawnLocation.x + 2, spawnLocation.z + 2) + " z:" + spawnLocation.z + 2)
+  // if (chest === undefined) {
+  //   console.warn("Could not load the position to place our chest.");
+  //   return -1;
+  // }
+
+  // chest.setPermutation(BlockPermutation.resolve("chest"));
+  world.afterEvents.playerPlaceBlock.subscribe(playerPlaceBlockAfterCallback)
   
 }
 
